b0c3320a0cc17f0d4627e377fe5ae48340956e94
Majharul-Munna <majharulmunna21@gmail.com>
2022-09-03 02:42:41 +0600

    third commit

--- js/get-news.js
+++ js/get-news.js
@@ -1,10 +1,32 @@
-const loadNews = () => {
-    fetch('https://openapi.programming-hero.com/api/news/category/01')
+const loadNews = (tap) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${tap}`
+    fetch(url)
     .then(res => res.json())
     .then(data => displayNews(data.data))
 }
 
 const displayNews = newss => {
+    console.log(newss);
+    const newsContainer = document.getElementById('news-container');
+    newss.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+              <div class="card container">
+                    <img src="${news.thumbnail_url}" class="" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${news.title}</h5>
+                    <p class="card-text">${news.details.slice(0,200)}</p>
+                    </div>
+                    <div class = "d-flex">
+                    <img src="${news.author}" class="img-fluid rounded float-start" alt="...">
+                    <p>${news.author.name}</p>
+                    </div>
+                    
+              </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    });
 }
 
-loadNews();
+loadNews('03');
